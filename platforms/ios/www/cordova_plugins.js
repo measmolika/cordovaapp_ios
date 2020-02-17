fb628e0a4955a758c3159f049eacc3d2d4f8e09e
Office MacBook Air <matchmysound@gmail.com>
2020-02-17 16:52:13 +0200

    Point to ttambet github version of cordova-background-video

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,24 +1,24 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-background-video.backgroundvideo",
-      "file": "plugins/cordova-background-video/www/backgroundvideo.js",
-      "pluginId": "cordova-background-video",
-      "clobbers": [
-        "cordova.plugins.backgroundvideo"
-      ]
-    },
-    {
       "id": "cordova-plugin-file-opener2.FileOpener2",
       "file": "plugins/cordova-plugin-file-opener2/www/plugins.FileOpener2.js",
       "pluginId": "cordova-plugin-file-opener2",
       "clobbers": [
         "cordova.plugins.fileOpener2"
       ]
+    },
+    {
+      "id": "cordova-background-video.backgroundvideo",
+      "file": "plugins/cordova-background-video/www/backgroundvideo.js",
+      "pluginId": "cordova-background-video",
+      "clobbers": [
+        "cordova.plugins.backgroundvideo"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-background-video": "1.0.21",
-    "cordova-plugin-file-opener2": "3.0.0"
+    "cordova-plugin-file-opener2": "3.0.0",
+    "cordova-background-video": "1.0.21"
   };
 });
